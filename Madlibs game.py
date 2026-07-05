dad3b66e501f57bd15b69815b6e524f4b73b15f1
Hardikkaushal-07 <lemelion007@gmail.com>
2026-07-06 01:16:55 +0530

    Add files via upload

---
+++ Madlibs game.py
@@ -0,0 +1,18 @@
+# This is a word game where u create a story by filling in the blanks with random words
+
+adjective = input("Give me an adjective:")
+animal = input("Give me an animal:")
+place = input("Give me a place:")
+adjective2 = input("Give me another adjective:")
+clothes = input("Give me an article of clothing:")
+noun = input("Give me a noun:")
+vehicle = input("Give me a vehicle:")
+place2 = input("Give me a place:")
+noun2 = input("Give me a noun:")
+
+
+print(f"It was a totally {adjective} morning when I decided to walk my pet {animal} to the {place}.")
+print(f"Suddenly, a {adjective2} stranger wearing a {clothes} ran up and shouted,")
+print(f"You have to help me find my missing {noun}!")
+print(f"Without thinking, I jumped onto my {vehicle} and we sped toward the {place2}.  When we arrived, we found it hiding behind a giant {noun2}.")
+print(f"At the end the stranger was so happy that he gave me a nice muffin")
