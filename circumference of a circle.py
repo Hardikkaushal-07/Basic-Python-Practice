31f3dbee9c6ab28ef292eda6d106364affd56f07
Hardikkaushal-07 <lemelion007@gmail.com>
2026-07-06 12:55:05 +0530

    Add files via upload

---
+++ circumference of a circle.py
@@ -0,0 +1,10 @@
+#calculating the circumference of a circle
+
+#importing maths
+import math
+
+Radius = float(input("What's the radius of the circle?"))
+result = 2 * math.pi * Radius
+
+# printing the result
+print(f"The circumference of your given circle is: {result}m")
